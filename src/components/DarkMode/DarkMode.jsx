1ceb3eae989945280a560bdd8f2f3f9cc941527b
Walid Ashraf <walidashraf050@gmail.com>
2025-06-23 15:32:09 +0300

    Fix DarkMode class in Tailwind

--- src/components/DarkMode/DarkMode.jsx
+++ src/components/DarkMode/DarkMode.jsx
@@ -1,16 +1,42 @@
 import { Moon, Sun } from "lucide-react";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const DarkMode = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
+  );
+
+  const element = document.documentElement;
+
+  useEffect(() => {
+    if (theme === "dark") {
+      element.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      element.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [theme]);
+
+  console.log(theme);
   return (
     <>
-      <button
-        className="cursor-pointer"
-        onClick={() => setDarkMode((prev) => !prev)}
-      >
-        {darkMode ? <Sun size={20} /> : <Moon size={20} />}
-      </button>
+      <div className="relative">
+        <button
+          className="cursor-pointer drop-shadow-[1px_1px_1px_rgba(0,0,0,0.1)] transition-all duration-300 absolute -bottom-3 right-0 z-10"
+          onClick={() =>
+            setTheme((data) => (data === "dark" ? "light" : "dark"))
+          }
+        >
+          {theme === "dark" ? (
+            <Sun size={20} />
+          ) : theme === "light" ? (
+            <Moon size={20} />
+          ) : (
+            ""
+          )}
+        </button>
+      </div>
     </>
   );
 };
